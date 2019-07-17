fe0b4e6a7e2a5d6e35c1f90c6ea825443855f445
Kyung Mo Kweon <kkweon@gmail.com>
2019-07-16 21:01:37 -0700

    Add ESLINT

--- script.js
+++ script.js
@@ -4,14 +4,16 @@
  * How to use:
  *
  * (0) Go to a markdown page (e.g., chapter2.md)
- * (1) Open Developer Console on the browser and Copy the code and Paste the output to the spreadsheet
+ * (1) Open Developer Console on the browser
+ * (2) Copy the code and Paste the output to the spreadsheet
  */
 function getTimeComplexity(anchor) {
   try {
     return anchor.parentElement.nextElementSibling.firstElementChild.textContent
       .replace(/time:/i, '')
       .trim();
-  } catch {
+  } catch (e) {
+    console.errror(e);
     return 'NA';
   }
 }
@@ -21,19 +23,22 @@ function getSpaceComplexity(anchor) {
     return anchor.parentElement.nextElementSibling.firstElementChild.nextElementSibling.textContent
       .replace(/space:/i, '')
       .trim();
-  } catch {
+  } catch (e) {
+    console.error(e);
     return 'NA';
   }
 }
 
-var anchors = [...document.querySelectorAll('.markdown-body h2 .anchor')];
-var ret = anchors
-  .map(a => {
-    const t = getTimeComplexity(a);
-    const s = getSpaceComplexity(a);
+{
+  const anchors = [...document.querySelectorAll('.markdown-body h2 .anchor')];
+  const ret = anchors
+    .map(a => {
+      const t = getTimeComplexity(a);
+      const s = getSpaceComplexity(a);
 
-    return `=HYPERLINK("${a.href}", "${t} / ${s}")`;
-  })
-  .join('\t');
+      return `=HYPERLINK("${a.href}", "${t} / ${s}")`;
+    })
+    .join('\t');
 
-console.log(ret);
+  console.log(ret);
+}
